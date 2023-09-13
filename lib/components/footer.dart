0037643958edaf501d472df1e1499e5b6618a8f1
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-09-13 09:18:06 -0300

    Update footer.dart

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -2,13 +2,15 @@ class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.white,
       padding: EdgeInsets.all(16),
-      child: Text(
-        'Rodapé Personalizado',
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.white,
+      child: Center(
+        child: Text(
+          'Copyright 2023 by HealthArch',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+          ),
         ),
       ),
     );
