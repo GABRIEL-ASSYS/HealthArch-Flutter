ef38becca73c8b29c39328d91d52a196b19ff226
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-09-14 11:00:14 -0300

    Update ConsultasScreen.dart

--- lib/screens/ConsultasScreen.dart
+++ lib/screens/ConsultasScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prova_01/screens/AdicionarConsultaScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ConsultasScreen extends StatefulWidget {
   @override
@@ -14,7 +15,17 @@ class _ConsultasScreenState extends State<ConsultasScreen> {
   TextEditingController nomeClienteController = TextEditingController();
   TextEditingController nomeProfissionalController = TextEditingController();
 
-  void adicionarConsulta() {
+  void adicionarConsulta() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    await firestore.collection('consultas').add({
+      'titulo': tituloController.text,
+      'descricao': descricaoController.text,
+      'horaData': horaDataController.text,
+      'nomeCliente': nomeClienteController.text,
+      'nomeProfissional': nomeProfissionalController.text,
+    });
+
     setState(() {
       consultas.add({
         'id': consultas.length + 1,
@@ -49,90 +60,6 @@ class _ConsultasScreenState extends State<ConsultasScreen> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 16.0),
-            TextField(
-              controller: tituloController,
-              decoration: InputDecoration(
-                labelText: 'Título:',
-              ),
-            ),
-            TextField(
-              controller: descricaoController,
-              decoration: InputDecoration(
-                labelText: 'Descrição:',
-              ),
-            ),
-            TextField(
-              controller: horaDataController,
-              decoration: InputDecoration(
-                labelText: 'Data e Hora:',
-              ),
-            ),
-            TextField(
-              controller: nomeClienteController,
-              decoration: InputDecoration(
-                labelText: 'Nome do Cliente:',
-              ),
-            ),
-            TextField(
-              controller: nomeProfissionalController,
-              decoration: InputDecoration(
-                labelText: 'Nome do Profissional:',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: adicionarConsulta,
-              child: Text('Cadastrar'),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Lista de Consultas:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: consultas.length,
-              itemBuilder: (context, index) {
-                final consulta = consultas[index];
-                return Card(
-                  elevation: 3.0,
-                  margin: EdgeInsets.symmetric(vertical: 8.0),
-                  child: ListTile(
-                    title: Text('ID: ${consulta['id']}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Título: ${consulta['titulo']}'),
-                        Text('Descrição: ${consulta['descricao']}'),
-                        Text('Data e Hora: ${consulta['horaData']}'),
-                        Text('Nome do Cliente: ${consulta['nomeCliente']}'),
-                        Text('Nome do Profissional: ${consulta['nomeProfissional']}'),
-                      ],
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              consultas.removeAt(index);
-                            });
-                          },
-                          child: Text('Excluir'),
-                        ),
-                        SizedBox(width: 8.0),
-                        ElevatedButton(
-                          onPressed: () {
-                          },
-                          child: Text('Editar'),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
           ],
         ),
       ),
@@ -140,7 +67,7 @@ class _ConsultasScreenState extends State<ConsultasScreen> {
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => AdicionarConsultaScreen())
+            MaterialPageRoute(builder: (context) => AdicionarConsultaScreen()),
           );
         },
         child: Icon(Icons.add),
