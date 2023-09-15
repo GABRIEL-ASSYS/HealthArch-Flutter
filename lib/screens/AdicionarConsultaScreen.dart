b7cc5122f3873d623194764e8620cf7cade2d8ef
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-09-15 08:56:48 -0300

    Update AdicionarConsultaScreen.dart

--- lib/screens/AdicionarConsultaScreen.dart
+++ lib/screens/AdicionarConsultaScreen.dart
@@ -22,47 +22,52 @@ class _AdicionarConsultaScreenState extends State<AdicionarConsultaScreen> {
         title: Text('Adicionar Consulta'),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Adicionar Consulta:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 400),
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Adicionar Consulta:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16.0),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: tituloController,
+                        decoration: InputDecoration(labelText: 'Título'),
+                      ),
+                      TextFormField(
+                        controller: descricaoController,
+                        decoration: InputDecoration(labelText: 'Descrição'),
+                      ),
+                      TextFormField(
+                        controller: horaDataController,
+                        decoration: InputDecoration(labelText: 'Data e Hora'),
+                      ),
+                      TextFormField(
+                        controller: nomeClienteController,
+                        decoration: InputDecoration(labelText: 'Nome do Cliente'),
+                      ),
+                      TextFormField(
+                        controller: nomeProfissionalController,
+                        decoration: InputDecoration(labelText: 'Nome do Profissional'),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: adicionarConsulta,
+                  child: Text('Cadastrar'),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: tituloController,
-                    decoration: InputDecoration(labelText: 'Título'),
-                  ),
-                  TextFormField(
-                    controller: descricaoController,
-                    decoration: InputDecoration(labelText: 'Descrição'),
-                  ),
-                  TextFormField(
-                    controller: horaDataController,
-                    decoration: InputDecoration(labelText: 'Data e Hora'),
-                  ),
-                  TextFormField(
-                    controller: nomeClienteController,
-                    decoration: InputDecoration(labelText: 'Nome do Cliente'),
-                  ),
-                  TextFormField(
-                    controller: nomeProfissionalController,
-                    decoration: InputDecoration(labelText: 'Nome do Profissional'),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: adicionarConsulta,
-              child: Text('Cadastrar'),
-            ),
-          ],
+          ),
         ),
       ),
     );
