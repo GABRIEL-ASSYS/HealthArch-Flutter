fc1bb75780a6175fa80b0244658277f6d72fda06
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-09-15 08:32:56 -0300

    configurando firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:prova_01/screens/ConsultasScreen.dart';
-import 'package:firebase_core/firebase_core.dart'; // Importe o Firebase
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Inicialize o Firebase
+  await Firebase.initializeApp(); 
   runApp(const MyApp());
 }
 
